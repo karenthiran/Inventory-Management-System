19860220b9eaace213edeb6f6abe3121e0c9fc48
karenthiran <kkarenthiran@gmail.com>
2026-02-25 23:40:45 +0530

    chore:add folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className='min-h-screen bg-gray-100 flex items-center justify-center text-2xl font-bold text-gray-800'>
-        Hello World
+        Inventory Management System
       </div>
     </>
   );
