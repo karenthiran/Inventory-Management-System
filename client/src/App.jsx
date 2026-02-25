b3fc5f7c8ebb6d9fd7ea9300d8263193e17f901a
karenthiran <kkarenthiran@gmail.com>
2026-02-25 22:38:03 +0530

    chore:client section redefiend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='min-h-screen bg-gray-100 flex items-center justify-center text-2xl font-bold text-gray-800'>
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
