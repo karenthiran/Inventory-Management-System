4b6b4d4ec808218a03c1c39492659172aa5f44ca
Anton Rex Nivethanan Croos <nivethanancroos14@gmail.com>
2026-02-26 22:10:00 +0530

    sidebar logo import end coded

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -22,10 +22,10 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className="w-64 min-h-screen bg-gray-100 p-6">
+    <div className="w-64 min-h-screen bg-gray-100 pt-0 px-6 pb-6">
       {/* logo */}
-      <div className="  flex justify-center">
-        <img src={logo} alt="IMS Logo" className="h-50 object-contain" />
+      <div className=" flex justify-center ">
+        <img src={logo} alt="IMS Logo" className="h-40 object-contain" />
       </div>
 
       <div className="space-y-2">
