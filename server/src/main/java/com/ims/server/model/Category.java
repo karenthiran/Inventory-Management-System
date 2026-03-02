7928a42ef1255d1569fd524c66d5e3b2cda168be
karenthiran <kkarenthiran@gmail.com>
2026-03-02 12:40:43 +0530

    fix(category): remove camelCase format conversion

--- server/src/main/java/com/ims/server/model/Category.java
+++ server/src/main/java/com/ims/server/model/Category.java
@@ -16,11 +16,9 @@ import lombok.NoArgsConstructor;
 public class Category {
 
     @Id
-    @Column(name = "id", length = 30)
+    @Column(length = 30)
     private String id;
 
-    // Use double quotes inside the name attribute to match
-    // case-sensitive columns in PostgreSQL exactly
-    @Column(name = "\"categoryName\"", nullable = false, length = 255)
+    @Column(name = "category_name", nullable = false, length = 255)
     private String categoryName;
 }
