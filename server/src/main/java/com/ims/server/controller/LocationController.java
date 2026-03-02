88f0b8398c1420aeb786cc5b12821711624f9e50
karenthiran <kkarenthiran@gmail.com>
2026-03-02 13:35:11 +0530

    feat(locationController): add controller for location

---
+++ server/src/main/java/com/ims/server/controller/LocationController.java
@@ -0,0 +1,29 @@
+package com.ims.server.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ims.server.model.Location;
+import com.ims.server.repository.LocationRepository;
+
+@RestController
+@RequestMapping("api/locations")
+@CrossOrigin(origins = "http://localhost:8080")
+public class LocationController {
+
+    private final LocationRepository locationRepository;
+
+    public LocationController(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+    @PostMapping("/add")
+    public Location createLoaction(@RequestBody Location location) {
+        // this takes the JSON from the user and saves it to the DB
+        return locationRepository.save(location);
+    }
+
+}
