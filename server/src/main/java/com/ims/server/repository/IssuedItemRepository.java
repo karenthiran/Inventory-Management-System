5deef82c80c06cd43b59279bb9872d19e5e4dcee
karenthiran <kkarenthiran@gmail.com>
2026-03-03 00:29:56 +0530

    feat(IssuedItemRepository): add issue item repository
    Created repository for managing issued items
    Enables CRUD operations and integration with service layer

---
+++ server/src/main/java/com/ims/server/repository/IssuedItemRepository.java
@@ -0,0 +1,11 @@
+package com.ims.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ims.server.model.IssuedItem;
+
+@Repository
+public interface IssuedItemRepository extends JpaRepository<IssuedItem, Long> {
+
+}
