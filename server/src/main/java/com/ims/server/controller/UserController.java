fdaf39d60ccdd89086e6d7d1b4b3cfaabb44d274
karenthiran <kkarenthiran@gmail.com>
2026-03-07 19:51:04 +0530

    feat(usercontroller): add get api call

--- server/src/main/java/com/ims/server/controller/UserController.java
+++ server/src/main/java/com/ims/server/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.ims.server.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,12 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
 
     @Autowired
     private PasswordEncoder passwordEncoder;
