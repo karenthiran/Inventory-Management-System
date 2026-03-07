701d9be2d71c7ca1384ec6f4214223f94434ce1b
karenthiran <kkarenthiran@gmail.com>
2026-03-07 23:54:02 +0530

    feat(userRepository): add check email exisit or not

--- server/src/main/java/com/ims/server/repository/UserRepository.java
+++ server/src/main/java/com/ims/server/repository/UserRepository.java
@@ -9,5 +9,10 @@ import com.ims.server.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    // Used for Login and Password Reset verification
     Optional<User> findByEmail(String email);
+
+    // ADD THIS: Used in /forgot-password to check if the user exists
+    boolean existsByEmail(String email);
 }
