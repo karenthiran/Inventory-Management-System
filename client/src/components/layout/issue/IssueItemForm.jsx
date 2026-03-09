c223107461a0ea184b6b870952defdd83c43c01a
Anton Rex Nivethanan Croos <nivethanancroos14@gmail.com>
2026-03-09 21:50:58 +0530

    import useref from react

--- client/src/components/layout/issue/IssueItemForm.jsx
+++ client/src/components/layout/issue/IssueItemForm.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { ChevronDown, X } from "lucide-react";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef } from "react";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
